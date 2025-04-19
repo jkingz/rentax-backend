9c50c40157ac6f6efa442eb1205b5f8e69d394fc
jkingz <jfkrule@gmail.com>
2025-04-19 10:05:37 +0800

    feat: Add update manager controller

--- src/controllers/managerControllers.ts
+++ src/controllers/managerControllers.ts
@@ -18,6 +18,7 @@ export const getManager = async (
       res.status(404).json({ message: 'manager not found' });
       return;
     }
+    res.status(200).json(manager);
   } catch (error: any) {
     res
       .status(500)
@@ -40,12 +41,37 @@ export const createManager = async (
         phoneNumber,
       },
     });
-
     res.status(201).json(manager);
   } catch (error: any) {
     res
       .status(500)
       .json({ message: `Error retrieving  manager: ${error.message}` });
+    return;
+  }
+};
+
+export const updateManager = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { cognitoId } = req.params;
+    const { name, email, phoneNumber } = req.body;
+    const updateManager = await prisma.manager.update({
+      where: {
+        cognitoId: cognitoId,
+      },
+      data: {
+        name,
+        email,
+        phoneNumber,
+      },
+    });
+    res.status(200).json(updateManager);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Failed to update manager: ${error.message}` });
     return;
   }
 };
